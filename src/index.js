3b581a5b621a53b453d25504d7d6209847882c60
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2021-02-08 12:02:54 -0500

    added toys index and styling and toy card

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 let addToy = false;
+const url = `http://localhost:3000/toys`
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
+const collection = document.querySelector('div#toy-collection')
+
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+  
+  
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -13,3 +18,34 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+  
+
+function renderAllToys() {
+  return fetch(url)
+  .then(res => res.json())
+  .then(toys => {
+    toys.forEach(toy => renderOneToy(toy))
+    // console.log(toy)
+})
+}
+
+function renderOneToy (toy){
+  const card = document.createElement('div')
+  card.className = 'toy-card'
+  card.dataset.id = toy.id
+  let image = document.createElement(`img`)
+  image.setAttribute('src', toy.image)
+  card.innerHTML =`
+  <div class="card">
+    <h2${toy.name}</h2>
+    <img src=${toy.image} class='toy-avatar' </>
+    <p> ${toy.likes}</p>
+    <button class="like-btn">Like<3</button>
+  </div> `
+
+collection.append(card)
+}
+renderAllToys()
+
+
